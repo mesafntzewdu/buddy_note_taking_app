6afed70392d1a0f84919b8db2d3e473aceb90a0c
mesafint <mesafntzewdu@gmail.com>
2024-04-07 05:57:11 +0300

    animation added

--- lib/screen/add_task.dart
+++ lib/screen/add_task.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class AddTask extends StatefulWidget {
   const AddTask({super.key});
@@ -17,6 +18,82 @@ class AddTaskState extends State<AddTask> {
         title: Text(
           'Add new Task',
           style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Lottie.asset(
+                  'assets/animation.json',
+                  width: double.infinity,
+                  height: 300,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'Task Title',
+                    label: Text(
+                      'Task Title',
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  maxLines: 3,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'Task Description',
+                    label: Text(
+                      'Task Description',
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Task End date',
+                    label: Text('Task End date'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Task Reminder',
+                    label: Text('Task Title'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  label: Text(
+                    'Add Task',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
